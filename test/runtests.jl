943853e2f7d77815bd2ad228740101910b0bd07c
Piotr Dobaczewski <pdobacz@gmail.com>
2016-10-13 23:00:10 +0200

    readable fixture in use

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,13 +9,19 @@ using DataFrames
 
 @fixture file function(filename) openxl(filename) end
 
-@fixture full_dfs function(file, filename)
-  ret = []
-  for f in [file, filename]
-    push!(ret, readxl(DataFrame, f, "Sheet1!C3:O7"))
-    push!(ret, readxlsheet(DataFrame, f, "Sheet1"))
+@fixture readable params=[:filename, :file] function(request, filename, file)
+  # FIXME: uglish
+  if request.param == :filename
+    filename
+  elseif request.param == :file
+    file
   end
-  ret
+end
+
+@fixture full_dfs function(readable)
+  [ readxl(DataFrame, readable, "Sheet1!C3:O7"),
+    readxlsheet(DataFrame, readable, "Sheet1")
+  ]
 end
 
 @fixture good_colnames function()
@@ -46,103 +52,135 @@ end
     end
   end
 
-  @pytest function reading_toarray(file, filename)
+  @pytest function reading_toarray(readable)
 
-    for f in [file, filename]
-        @test_throws ErrorException readxl(f, "Sheet1!C4:G3")
-        @test_throws ErrorException readxl(f, "Sheet1!G2:B5")
-        @test_throws ErrorException readxl(f, "Sheet1!G5:B2")
+    @test_throws ErrorException readxl(readable, "Sheet1!C4:G3")
+    @test_throws ErrorException readxl(readable, "Sheet1!G2:B5")
+    @test_throws ErrorException readxl(readable, "Sheet1!G5:B2")
 
-        data = readxl(f, "Sheet1!C3:N7")
-        @test size(data) == (5,12)
-        @test data[4,1] == 2.0
-        @test data[2,2] == "A"
-        @test data[2,3] == true
-        @test isna(data[4,5])
-        @test data[2,9] == Date(2015,3,3)
-        @test data[3,9] == DateTime(2015,2,4,10,14)
-        @test data[4,9] == DateTime(1988,4,9,0,0)
-        @test data[5,9] == ExcelReaders.Time(15,2,0)
-        @test data[3,10] == DateTime(1950,8,9,18,40)
-        @test isna(data[5,10])
-        @test isa(data[2,11], ExcelErrorCell)
-        @test isa(data[3,11], ExcelErrorCell)
-        @test isa(data[4,12], ExcelErrorCell)
-        @test isna(data[5,12])
-    end
+    data = readxl(readable, "Sheet1!C3:N7")
+    @test size(data) == (5,12)
+    @test data[4,1] == 2.0
+    @test data[2,2] == "A"
+    @test data[2,3] == true
+    @test isna(data[4,5])
+    @test data[2,9] == Date(2015,3,3)
+    @test data[3,9] == DateTime(2015,2,4,10,14)
+    @test data[4,9] == DateTime(1988,4,9,0,0)
+    @test data[5,9] == ExcelReaders.Time(15,2,0)
+    @test data[3,10] == DateTime(1950,8,9,18,40)
+    @test isna(data[5,10])
+    @test isa(data[2,11], ExcelErrorCell)
+    @test isa(data[3,11], ExcelErrorCell)
+    @test isa(data[4,12], ExcelErrorCell)
+    @test isna(data[5,12])
   end
 
   @pytest function reading_todf(full_dfs)
 
-        for df in full_dfs
-            @test ncol(df) == 13
-            @test nrow(df) == 4
-            @test isa(df[Symbol("Some Float64s")], DataVector{Float64})
-            @test isa(df[Symbol("Some Strings")], DataVector{Compat.UTF8String})
-            @test isa(df[Symbol("Some Bools")], DataVector{Bool})
-            @test isa(df[Symbol("Mixed column")], DataVector{Any})
-            @test isa(df[Symbol("Mixed with NA")], DataVector{Any})
-            @test isa(df[Symbol("Some dates")], DataVector{Any})
-            @test isa(df[Symbol("Dates with NA")], DataVector{Any})
-            @test df[4,Symbol("Some Float64s")] == 2.5
-            @test df[4,Symbol("Some Strings")] == "DDDD"
-            @test df[4,Symbol("Some Bools")] == true
-            @test df[1,Symbol("Mixed column")] == 2.0
-            @test df[2,Symbol("Mixed column")] == "EEEEE"
-            @test df[3,Symbol("Mixed column")] == false
-            @test isna(df[3,Symbol("Mixed with NA")])
-            @test df[1,Symbol("Float64 with NA")] == 3.
-            @test isna(df[2,Symbol("Float64 with NA")])
-            @test df[1,Symbol("String with NA")] == "FF"
-            @test isna(df[2,Symbol("String with NA")])
-            @test df[2,Symbol("Bool with NA")] == true
-            @test isna(df[1,Symbol("Bool with NA")])
-            @test df[1,Symbol("Dates with NA")] == Date(1965,4,3)
-            @test df[2,Symbol("Some dates")] == DateTime(2015,2,4,10,14)
-            @test df[4,Symbol("Some dates")] == ExcelReaders.Time(15,2,0)
-            @test isna(df[4,Symbol("Dates with NA")])
-            # TODO Add a test that checks the error code, not just type
-            @test isa(df[1,Symbol("Some errors")], ExcelErrorCell)
-            @test isna(df[4,Symbol("Errors with NA")])
-        end
-
-  end
-
-  @pytest function reading_noheader(file, filename, good_colnames)
-
-    for f in [file, filename]
-        df = readxl(DataFrame, f, "Sheet1!C4:O7", header=false)
+    for df in full_dfs
         @test ncol(df) == 13
         @test nrow(df) == 4
-        @test isa(df[1], DataVector{Float64})
-        @test isa(df[2], DataVector{Compat.UTF8String})
-        @test isa(df[3], DataVector{Bool})
-        @test isa(df[4], DataVector{Any})
-        @test isa(df[5], DataVector{Any})
-        @test isa(df[9], DataVector{Any})
-        @test isa(df[10], DataVector{Any})
-        @test df[4,1] == 2.5
-        @test df[4,2] == "DDDD"
-        @test df[4,3] == true
-        @test df[1,4] == 2.0
-        @test df[2,4] == "EEEEE"
-        @test df[3,4] == false
-        @test isna(df[3,5])
-        @test df[1,6] == 3.
-        @test isna(df[2,6])
-        @test df[1,7] == "FF"
-        @test isna(df[2,7])
-        @test df[2,8] == true
-        @test isna(df[1,8])
-        @test df[1,10] == Date(1965,4,3)
-        @test df[2,9] == DateTime(2015,2,4,10,14)
-        @test df[4,9] == ExcelReaders.Time(15,2,0)
-        @test isna(df[4,10])
+        @test isa(df[Symbol("Some Float64s")], DataVector{Float64})
+        @test isa(df[Symbol("Some Strings")], DataVector{Compat.UTF8String})
+        @test isa(df[Symbol("Some Bools")], DataVector{Bool})
+        @test isa(df[Symbol("Mixed column")], DataVector{Any})
+        @test isa(df[Symbol("Mixed with NA")], DataVector{Any})
+        @test isa(df[Symbol("Some dates")], DataVector{Any})
+        @test isa(df[Symbol("Dates with NA")], DataVector{Any})
+        @test df[4,Symbol("Some Float64s")] == 2.5
+        @test df[4,Symbol("Some Strings")] == "DDDD"
+        @test df[4,Symbol("Some Bools")] == true
+        @test df[1,Symbol("Mixed column")] == 2.0
+        @test df[2,Symbol("Mixed column")] == "EEEEE"
+        @test df[3,Symbol("Mixed column")] == false
+        @test isna(df[3,Symbol("Mixed with NA")])
+        @test df[1,Symbol("Float64 with NA")] == 3.
+        @test isna(df[2,Symbol("Float64 with NA")])
+        @test df[1,Symbol("String with NA")] == "FF"
+        @test isna(df[2,Symbol("String with NA")])
+        @test df[2,Symbol("Bool with NA")] == true
+        @test isna(df[1,Symbol("Bool with NA")])
+        @test df[1,Symbol("Dates with NA")] == Date(1965,4,3)
+        @test df[2,Symbol("Some dates")] == DateTime(2015,2,4,10,14)
+        @test df[4,Symbol("Some dates")] == ExcelReaders.Time(15,2,0)
+        @test isna(df[4,Symbol("Dates with NA")])
         # TODO Add a test that checks the error code, not just type
-        @test isa(df[1,11], ExcelErrorCell)
-        @test isna(df[4,12])
+        @test isa(df[1,Symbol("Some errors")], ExcelErrorCell)
+        @test isna(df[4,Symbol("Errors with NA")])
+    end
+  end
 
-        df = readxl(DataFrame, f, "Sheet1!C4:O7", header=false, colnames=good_colnames)
+  @pytest function reading_noheader(readable, good_colnames)
+
+    df = readxl(DataFrame, readable, "Sheet1!C4:O7", header=false)
+    @test ncol(df) == 13
+    @test nrow(df) == 4
+    @test isa(df[1], DataVector{Float64})
+    @test isa(df[2], DataVector{Compat.UTF8String})
+    @test isa(df[3], DataVector{Bool})
+    @test isa(df[4], DataVector{Any})
+    @test isa(df[5], DataVector{Any})
+    @test isa(df[9], DataVector{Any})
+    @test isa(df[10], DataVector{Any})
+    @test df[4,1] == 2.5
+    @test df[4,2] == "DDDD"
+    @test df[4,3] == true
+    @test df[1,4] == 2.0
+    @test df[2,4] == "EEEEE"
+    @test df[3,4] == false
+    @test isna(df[3,5])
+    @test df[1,6] == 3.
+    @test isna(df[2,6])
+    @test df[1,7] == "FF"
+    @test isna(df[2,7])
+    @test df[2,8] == true
+    @test isna(df[1,8])
+    @test df[1,10] == Date(1965,4,3)
+    @test df[2,9] == DateTime(2015,2,4,10,14)
+    @test df[4,9] == ExcelReaders.Time(15,2,0)
+    @test isna(df[4,10])
+    # TODO Add a test that checks the error code, not just type
+    @test isa(df[1,11], ExcelErrorCell)
+    @test isna(df[4,12])
+
+    df = readxl(DataFrame, readable, "Sheet1!C4:O7", header=false, colnames=good_colnames)
+    @test ncol(df) == 13
+    @test nrow(df) == 4
+    @test isa(df[:c1], DataVector{Float64})
+    @test isa(df[:c2], DataVector{Compat.UTF8String})
+    @test isa(df[:c3], DataVector{Bool})
+    @test isa(df[:c4], DataVector{Any})
+    @test isa(df[:c5], DataVector{Any})
+    @test isa(df[:c9], DataVector{Any})
+    @test isa(df[:c10], DataVector{Any})
+    @test df[4,:c1] == 2.5
+    @test df[4,:c2] == "DDDD"
+    @test df[4,:c3] == true
+    @test df[1,:c4] == 2.0
+    @test df[2,:c4] == "EEEEE"
+    @test df[3,:c4] == false
+    @test isna(df[3,:c5])
+    @test df[1,:c6] == 3.
+    @test isna(df[2,:c6])
+    @test df[1,:c7] == "FF"
+    @test isna(df[2,:c7])
+    @test df[2,:c8] == true
+    @test isna(df[1,:c8])
+    @test df[1,:c10] == Date(1965,4,3)
+    @test df[2,:c9] == DateTime(2015,2,4,10,14)
+    @test df[4,:c9] == ExcelReaders.Time(15,2,0)
+    @test isna(df[4,:c10])
+    # TODO Add a test that checks the error code, not just type
+    @test isa(df[1,:c11], ExcelErrorCell)
+    @test isna(df[4,:c12])
+  end
+
+
+  @pytest function overriding_colnames(readable, good_colnames)
+    full_dfs = [readxl(DataFrame, readable, "Sheet1!C3:O7", header=true, colnames=good_colnames),
+                readxlsheet(DataFrame, readable, "Sheet1", header=true, colnames=good_colnames)]
+    for df in full_dfs
         @test ncol(df) == 13
         @test nrow(df) == 4
         @test isa(df[:c1], DataVector{Float64})
@@ -169,137 +207,90 @@ end
         @test df[2,:c9] == DateTime(2015,2,4,10,14)
         @test df[4,:c9] == ExcelReaders.Time(15,2,0)
         @test isna(df[4,:c10])
-        # TODO Add a test that checks the error code, not just type
         @test isa(df[1,:c11], ExcelErrorCell)
         @test isna(df[4,:c12])
     end
   end
 
-
-  @pytest function overriding_colnames(file, filename, good_colnames)
-    for f in [file, filename]
-        full_dfs = [readxl(DataFrame, f, "Sheet1!C3:O7", header=true, colnames=good_colnames),
-                    readxlsheet(DataFrame, f, "Sheet1", header=true, colnames=good_colnames)]
-        for df in full_dfs
-            @test ncol(df) == 13
-            @test nrow(df) == 4
-            @test isa(df[:c1], DataVector{Float64})
-            @test isa(df[:c2], DataVector{Compat.UTF8String})
-            @test isa(df[:c3], DataVector{Bool})
-            @test isa(df[:c4], DataVector{Any})
-            @test isa(df[:c5], DataVector{Any})
-            @test isa(df[:c9], DataVector{Any})
-            @test isa(df[:c10], DataVector{Any})
-            @test df[4,:c1] == 2.5
-            @test df[4,:c2] == "DDDD"
-            @test df[4,:c3] == true
-            @test df[1,:c4] == 2.0
-            @test df[2,:c4] == "EEEEE"
-            @test df[3,:c4] == false
-            @test isna(df[3,:c5])
-            @test df[1,:c6] == 3.
-            @test isna(df[2,:c6])
-            @test df[1,:c7] == "FF"
-            @test isna(df[2,:c7])
-            @test df[2,:c8] == true
-            @test isna(df[1,:c8])
-            @test df[1,:c10] == Date(1965,4,3)
-            @test df[2,:c9] == DateTime(2015,2,4,10,14)
-            @test df[4,:c9] == ExcelReaders.Time(15,2,0)
-            @test isna(df[4,:c10])
-            @test isa(df[1,:c11], ExcelErrorCell)
-            @test isna(df[4,:c12])
-        end
-    end
-  end
-
-  @pytest function toofewcolnames(file, filename)
-    for f in [file, filename]
-      @test_throws ErrorException df = readxl(DataFrame, f, "Sheet1!C3:N7", header=true, colnames=[:c1, :c2, :c3, :c4])
-    end
+  @pytest function toofewcolnames(readable)
+    @test_throws ErrorException df = readxl(DataFrame, readable, "Sheet1!C3:N7", header=true, colnames=[:c1, :c2, :c3, :c4])
   end
 
       # Test readxlsheet function
-  @pytest function read_empty_xlsheet(file, filename)
-    for f in [file, filename]
-      @test_throws ErrorException readxlsheet(f, "Empty Sheet")
+  @pytest function read_empty_xlsheet(readable)
+    @test_throws ErrorException readxlsheet(readable, "Empty Sheet")
+  end
+
+  @pytest function readxlsheet_bad_dims(readable)
+    for sheetinfo=["Second Sheet", 2]
+        @test_throws ErrorException readxlsheet(readable, sheetinfo, skipstartrows=-1)
+        @test_throws ErrorException readxlsheet(readable, sheetinfo, skipstartrows=:nonsense)
+
+        @test_throws ErrorException readxlsheet(readable, sheetinfo, skipstartcols=-1)
+        @test_throws ErrorException readxlsheet(readable, sheetinfo, skipstartcols=:nonsense)
+
+        @test_throws ErrorException readxlsheet(readable, sheetinfo, nrows=-1)
+        @test_throws ErrorException readxlsheet(readable, sheetinfo, nrows=:nonsense)
+
+        @test_throws ErrorException readxlsheet(readable, sheetinfo, ncols=-1)
+        @test_throws ErrorException readxlsheet(readable, sheetinfo, ncols=:nonsense)
     end
   end
 
-  @pytest function readxlsheet_bad_dims(file, filename)
-    for f in [file, filename]
-      for sheetinfo=["Second Sheet", 2]
-          @test_throws ErrorException readxlsheet(f, sheetinfo, skipstartrows=-1)
-          @test_throws ErrorException readxlsheet(f, sheetinfo, skipstartrows=:nonsense)
+  @pytest function readxlsheet_succesful(readable)
+    for sheetinfo=["Second Sheet", 2]
+        data = readxlsheet(readable, sheetinfo)
+        @test size(data) == (6, 6)
+        @test data[2,1] == 1.
+        @test data[5,2] == "CCC"
+        @test data[3,3] == false
+        @test data[6,6] == ExcelReaders.Time(15,2,00)
+        @test isna(data[4,3])
+        @test isna(data[4,6])
 
-          @test_throws ErrorException readxlsheet(f, sheetinfo, skipstartcols=-1)
-          @test_throws ErrorException readxlsheet(f, sheetinfo, skipstartcols=:nonsense)
+        data = readxlsheet(readable, sheetinfo, skipstartrows=:blanks, skipstartcols=:blanks)
+        @test size(data) == (6, 6)
+        @test data[2,1] == 1.
+        @test data[5,2] == "CCC"
+        @test data[3,3] == false
+        @test data[6,6] == ExcelReaders.Time(15,2,00)
+        @test isna(data[4,3])
+        @test isna(data[4,6])
 
-          @test_throws ErrorException readxlsheet(f, sheetinfo, nrows=-1)
-          @test_throws ErrorException readxlsheet(f, sheetinfo, nrows=:nonsense)
+        data = readxlsheet(readable, sheetinfo, skipstartrows=0, skipstartcols=0)
+        @test size(data) == (6+7, 6+3)
+        @test data[2+7,1+3] == 1.
+        @test data[5+7,2+3] == "CCC"
+        @test data[3+7,3+3] == false
+        @test data[6+7,6+3] == ExcelReaders.Time(15,2,00)
+        @test isna(data[4+7,3+3])
+        @test isna(data[4+7,6+3])
 
-          @test_throws ErrorException readxlsheet(f, sheetinfo, ncols=-1)
-          @test_throws ErrorException readxlsheet(f, sheetinfo, ncols=:nonsense)
-      end
-    end
-  end
+        data = readxlsheet(readable, sheetinfo, skipstartrows=0, )
+        @test size(data) == (6+7, 6)
+        @test data[2+7,1] == 1.
+        @test data[5+7,2] == "CCC"
+        @test data[3+7,3] == false
+        @test data[6+7,6] == ExcelReaders.Time(15,2,00)
+        @test isna(data[4+7,3])
+        @test isna(data[4+7,6])
 
-  @pytest function readxlsheet_succesful(file, filename)
-    for f in [file, filename]
-      for sheetinfo=["Second Sheet", 2]
-          data = readxlsheet(f, sheetinfo)
-          @test size(data) == (6, 6)
-          @test data[2,1] == 1.
-          @test data[5,2] == "CCC"
-          @test data[3,3] == false
-          @test data[6,6] == ExcelReaders.Time(15,2,00)
-          @test isna(data[4,3])
-          @test isna(data[4,6])
+        data = readxlsheet(readable, sheetinfo, skipstartcols=0)
+        @test size(data) == (6, 6+3)
+        @test data[2,1+3] == 1.
+        @test data[5,2+3] == "CCC"
+        @test data[3,3+3] == false
+        @test data[6,6+3] == ExcelReaders.Time(15,2,00)
+        @test isna(data[4,3+3])
+        @test isna(data[4,6+3])
 
-          data = readxlsheet(f, sheetinfo, skipstartrows=:blanks, skipstartcols=:blanks)
-          @test size(data) == (6, 6)
-          @test data[2,1] == 1.
-          @test data[5,2] == "CCC"
-          @test data[3,3] == false
-          @test data[6,6] == ExcelReaders.Time(15,2,00)
-          @test isna(data[4,3])
-          @test isna(data[4,6])
-
-          data = readxlsheet(f, sheetinfo, skipstartrows=0, skipstartcols=0)
-          @test size(data) == (6+7, 6+3)
-          @test data[2+7,1+3] == 1.
-          @test data[5+7,2+3] == "CCC"
-          @test data[3+7,3+3] == false
-          @test data[6+7,6+3] == ExcelReaders.Time(15,2,00)
-          @test isna(data[4+7,3+3])
-          @test isna(data[4+7,6+3])
-
-          data = readxlsheet(f, sheetinfo, skipstartrows=0, )
-          @test size(data) == (6+7, 6)
-          @test data[2+7,1] == 1.
-          @test data[5+7,2] == "CCC"
-          @test data[3+7,3] == false
-          @test data[6+7,6] == ExcelReaders.Time(15,2,00)
-          @test isna(data[4+7,3])
-          @test isna(data[4+7,6])
-
-          data = readxlsheet(f, sheetinfo, skipstartcols=0)
-          @test size(data) == (6, 6+3)
-          @test data[2,1+3] == 1.
-          @test data[5,2+3] == "CCC"
-          @test data[3,3+3] == false
-          @test data[6,6+3] == ExcelReaders.Time(15,2,00)
-          @test isna(data[4,3+3])
-          @test isna(data[4,6+3])
-
-          data = readxlsheet(f, sheetinfo, skipstartrows=1, skipstartcols=1, nrows=11, ncols=7)
-          @test size(data) == (11, 7)
-          @test data[2+6,1+2] == 1.
-          @test data[5+6,2+2] == "CCC"
-          @test data[3+6,3+2] == false
-          @test_throws BoundsError data[6+6,6+2] == ExcelReaders.Time(15,2,00)
-          @test isna(data[4+6,2+2])
-      end
+        data = readxlsheet(readable, sheetinfo, skipstartrows=1, skipstartcols=1, nrows=11, ncols=7)
+        @test size(data) == (11, 7)
+        @test data[2+6,1+2] == 1.
+        @test data[5+6,2+2] == "CCC"
+        @test data[3+6,3+2] == false
+        @test_throws BoundsError data[6+6,6+2] == ExcelReaders.Time(15,2,00)
+        @test isna(data[4+6,2+2])
     end
   end
 
